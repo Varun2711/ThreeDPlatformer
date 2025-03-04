efd4ce32553f60c64d3d542cf3b2ab72d0d0c786
Varun Patel <rvjpatel99@gmail.com>
2025-03-03 21:49:46 -0800

    added player movement
    Also for the initial commit, it is quite heavy since I had to restart assignment, due to file corruption, and just forgot the commit before I added a lot of components

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,7 +3,7 @@ using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
-    UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
+    public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +13,25 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // movement input controls
+        Vector3 inputDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputDirection += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputDirection += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputDirection += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputDirection += Vector3.left;
+        }
+
+        OnMove?.Invoke(inputDirection);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private InputManager inputManager;
     [SerializeField] private CinemachineCamera camera;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed = 5f;
+
+    private Vector3 moveDir = Vector3.zero;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        inputManager.OnMove.AddListener(MovePlayer);
+
     }
 
     // Update is called once per frame
@@ -17,4 +24,21 @@ public class PlayerController : MonoBehaviour
     {
         transform.rotation = Quaternion.Euler(0, camera.transform.rotation.eulerAngles.y, 0);
     }
+
+    void MovePlayer(Vector3 direction)
+    {
+        Vector3 cf = camera.transform.forward;
+        Vector3 cr = camera.transform.right;
+        cf.y = 0;
+        cr.y = 0;
+        moveDir = cf * direction.z + cr * direction.x;
+        rb.linearVelocity = new Vector3(moveDir.x * speed, rb.linearVelocity.y, moveDir.z * speed);
+    }
+
+    //private void FixedUpdate()
+    //{
+        
+    //}
+
+
 }
