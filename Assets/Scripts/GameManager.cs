6fd60f18df20bec0344b8731d62299249b987f8e
Varun Patel <rvjpatel99@gmail.com>
2025-03-03 23:55:57 -0800

    added game mechanics and game ui

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,34 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private int score = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void incrementScore()
+    {
+        score++;
+        scoreText.text = $"Score: {score}";
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
